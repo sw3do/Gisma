34af9753d53e186def3614976651678d801d4111
Mert <89777525+sw3do@users.noreply.github.com>
2025-11-05 17:52:10 +0100

    .

---
+++ Data and algorithms/Lecture 3/count_sorting.py
@@ -0,0 +1,15 @@
+def countingSort(arr):
+    maxVal = max(arr)
+    count = [0] * (maxVal + 1)
+    
+    for num in arr:
+        count[num] += 1
+    
+    sorted_arr = []
+    for i in range(len(count)):
+        sorted_arr.extend([i] * count[i])
+    
+    return sorted_arr
+
+arr = [4, 2, 2, 8, 3, 3, 1]
+print(countingSort(arr))
